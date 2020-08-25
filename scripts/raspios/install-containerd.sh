2d7ecab90f5f0b54d0924796fcd5605d24336ba1
Johan Siebens <johan.siebens@gmail.com>
2020-08-25 07:58:43 +0200

    upgrade containerd to 1.3.5
    Signed-off-by: Johan Siebens <johan.siebens@gmail.com>

--- scripts/raspios/install-containerd.sh
+++ scripts/raspios/install-containerd.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "=> Downloading and installing containerd"
-curl -sSL https://github.com/alexellis/containerd-armhf/releases/download/v1.3.2/containerd.tgz | tar -xvz --strip-components=2 -C /usr/local/bin/
-curl -SLfs "https://raw.githubusercontent.com/containerd/containerd/v1.3.2/containerd.service" \
+curl -sSL https://github.com/alexellis/containerd-armhf/releases/download/v1.3.5/containerd-1.3.5-linux-armhf.tar.gz | tar -xvz --strip-components=1 -C /usr/local/bin/
+curl -SLfs "https://raw.githubusercontent.com/containerd/containerd/v1.3.5/containerd.service" \
     --output "/etc/systemd/system/containerd.service" \
     && chmod 0644 /etc/systemd/system/containerd.service
 systemctl enable containerd
